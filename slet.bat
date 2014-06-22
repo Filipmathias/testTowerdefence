87ed74ca9a98c2bb7850f0dd8f135fc8eabfda34
Fynfner <kennetnlarsen@gmail.com>
2014-06-22 21:31:42 +0200

    slet.bat

---
+++ slet.bat
@@ -0,0 +1,4 @@
+@RD /S /Q "C:\Users\Kennet\Documents\GitHub\TestTowerdefence\TowerDefence\TowerDefenceContent\bin"
+@RD /S /Q "C:\Users\Kennet\Documents\GitHub\TestTowerdefence\TowerDefence\TowerDefenceContent\obj"
+@RD /S /Q "C:\Users\Kennet\Documents\GitHub\TestTowerdefence\TowerDefence\TowerDefence\bin"
+@RD /S /Q "C:\Users\Kennet\Documents\GitHub\TestTowerdefence\TowerDefence\TowerDefence\obj"
