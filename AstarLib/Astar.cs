b9b0e3293293c3efc1c7e3d0456b8e5c2cc70b7d
Filip Jørgensen <Filipmathias@live.dk>
2014-08-21 11:58:38 +0200

    Beginings of the new world (struktur ændringer og Waves begyndelse)

--- AstarLib/Astar.cs
+++ AstarLib/Astar.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.GamerServices;
 namespace AstarLib
 {
     public class Astar
     {
-        byte[] map;
-        byte X;
-        byte Y;
-        
-        public Astar(byte Y, byte X, byte[] Map,int Start,int End) 
+        byte[] _map;
+        int _width;
+        int _height;
+        int _start;
+        int _end;
+
+
+        public Astar(int width, int height, byte[] Map, int start, int end) 
         {
-                
-        }
+        
+        }  
+
     }
 
 
---
+++ TowerDefence/TowerDefence/Game/WaveHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace TowerDefence.Game
+{
+    class WaveHandler
+    {
+        Queue<Wave> _Waves = new Queue<Wave>();
+        public Queue<Wave> Waves
+        {
+            get { return _Waves; }
+            set { _Waves = value; }
+        }
+
+        Wave _current;
+        public void WaveHandler() 
+        {
+
+        }
+
+
+
+    }
+    class Wave
+    {
+    
+    }
+
+
+}
